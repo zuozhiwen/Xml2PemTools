da218c80327bc82a3c96d7d78b5a8e155bed0ccb
zhiwen.zuo <zhiwen.zuo@aihuishou.com>
2018-02-02 14:15:05 +0800

    添加项目文件。

---
+++ Xml2PemTools/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+
+namespace Xml2PemTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pem2XmlUtility.PemConvertToXml(@"D:\PrivateKey.pem", @"D:\PrivateKey.xml");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pem2XmlUtility.XmlConvertToPem(@"D:\PrivateKey.xml", @"D:\PrivateKey2.pem");
+        }
+
+        // ref https://www.jianshu.com/p/faefcc58c79b
+    }
+}
---
+++ Xml2PemTools/Pem2XmlUtility.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+
+namespace Xml2PemTools
+{
+    internal static class Pem2XmlUtility
+    {
+        public static void XmlConvertToPem(string xmlPath, string pemPath)//XML格式密钥转PEM
+        {
+            var rsa2 = new RSACryptoServiceProvider();
+            using (var sr = new StreamReader(xmlPath))
+            {
+                rsa2.FromXmlString(sr.ReadToEnd());
+            }
+            var p = rsa2.ExportParameters(true);
+
+            var key = new RsaPrivateCrtKeyParameters(
+                new BigInteger(1, p.Modulus), new BigInteger(1, p.Exponent), new BigInteger(1, p.D),
+                new BigInteger(1, p.P), new BigInteger(1, p.Q), new BigInteger(1, p.DP), new BigInteger(1, p.DQ),
+                new BigInteger(1, p.InverseQ));
+
+            using (var sw = new StreamWriter(pemPath))
+            {
+                var pemWriter = new Org.BouncyCastle.OpenSsl.PemWriter(sw);
+                pemWriter.WriteObject(key);
+            }
+        }
+
+        public static void PemConvertToXml(string pemPath, string xmlPath)//PEM格式密钥转XML
+        {
+            AsymmetricCipherKeyPair keyPair;
+            using (var sr = new StreamReader(pemPath))
+            {
+                var pemReader = new Org.BouncyCastle.OpenSsl.PemReader(sr);
+                keyPair = (AsymmetricCipherKeyPair)pemReader.ReadObject();
+            }
+            var key = (RsaPrivateCrtKeyParameters)keyPair.Private;
+            var p = new RSAParameters
+            {
+                Modulus = key.Modulus.ToByteArrayUnsigned(),
+                Exponent = key.PublicExponent.ToByteArrayUnsigned(),
+                D = key.Exponent.ToByteArrayUnsigned(),
+                P = key.P.ToByteArrayUnsigned(),
+                Q = key.Q.ToByteArrayUnsigned(),
+                DP = key.DP.ToByteArrayUnsigned(),
+                DQ = key.DQ.ToByteArrayUnsigned(),
+                InverseQ = key.QInv.ToByteArrayUnsigned(),
+            };
+            var rsa = new RSACryptoServiceProvider();
+            rsa.ImportParameters(p);
+            using (var sw = new StreamWriter(xmlPath))
+            {
+                sw.Write(rsa.ToXmlString(true));
+            }
+        }
+    }
+}
