8315e9bb55ff66b6776eb901732d3c1b2252a4f9
zuozhiwen <zzwqqmail@qq.com>
2018-03-08 15:42:36 +0800

    update

--- Xml2PemTools/Form1.Designer.cs
+++ Xml2PemTools/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 42);
+            this.button1.Location = new System.Drawing.Point(58, 42);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(116, 31);
             this.button1.TabIndex = 0;
@@ -48,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(223, 42);
+            this.button2.Location = new System.Drawing.Point(206, 42);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(116, 31);
             this.button2.TabIndex = 1;
