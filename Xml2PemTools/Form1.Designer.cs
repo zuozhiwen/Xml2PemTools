2e87ea24d710cab2889dccee27ea386407ed9432
zuozhiwen <zzwqqmail@qq.com>
2018-03-08 14:50:48 +0800

    update

--- Xml2PemTools/Form1.Designer.cs
+++ Xml2PemTools/Form1.Designer.cs
@@ -82,6 +82,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Xml<--->Pem Tools";
             this.ResumeLayout(false);
 
